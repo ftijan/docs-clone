7aedbf566cbe09c67d4792fa050a6cf88135b322
Filip Tijan <ftijan@gmail.com>
2022-07-24 20:18:14 +0100

    adding document routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,27 @@
 import TextEditor from "./TextEditor";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate
+} from 'react-router-dom'
+import { v4 as uuidV4 } from 'uuid'
 
 function App() {
   return (
-    <TextEditor />
+    <Router>
+      <Routes>
+        <Route 
+          path="/" 
+          element={<Navigate to={`/documents/${uuidV4()}`} />} 
+          exact
+        />
+        <Route
+          path="/documents/:id"
+          element={<TextEditor />}
+        />
+      </Routes>
+    </Router>
   );
 }
 
